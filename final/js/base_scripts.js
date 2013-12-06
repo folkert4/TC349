fda46f17ba5cd309048bb9e6379e471140c7df15
folkert4 <folkert4@msu.edu>
2013-12-06 16:56:52 -0500

    housekeeping, favico, scripts, index init

---
+++ final/js/base_scripts.js
@@ -0,0 +1,44 @@
+var COOKIE = {
+	setCookie: function(name, value, expire) {
+		'use strict';
+		var str = encodeURIComponent(name) + '=' + encodeURIComponent(value);
+		str += ';expires=' + expire.toGMTString();
+		document.cookie = str;
+	},
+	getCookie: function(name) {
+		'use strict';
+		var len = name.length;
+		var cookies = document.cookie.split(';');
+		for (var i = 0, count = cookies.length; i < count; i++) {
+			var value = (cookies[i].slice(0,1) == ' ') ? cookies[i].slice(1) : cookies[i];
+			value = decodeURIComponent(value);
+			console.log(value.slice(0,len));
+			if (value.slice(0,len) == name) {
+				return cookies[i].split('=')[1];
+			}
+		}
+		return false;
+	},
+	deleteCookie: function(name) {
+		'use strict';
+		document.cookie = encodeURIComponent(name) + '=;expires=Thu, 01-Jan-1970 00:00:01 GMT';
+	}
+};
+
+function is_number(n) { return /^-?[\d.]+(?:e-?\d+)?$/.test(n); }
+
+function pass_index() {
+	window.location.replace('pitch.html');
+}
+
+function submit_draft() {
+	window.location.assign('draft2.html')
+}
+
+function submit_draft2() {
+	window.location.assign('draft3.html')
+}
+
+function submit_draft3() {
+	window.location.assign('draft.html')
+}
